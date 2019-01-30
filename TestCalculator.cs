02fb7aa37098fbb27531f1075a80911ee515c124
Pétur Freyr Kristmundsson <petur@strikamerki.is>
2019-01-30 15:32:28 +0000

    Made a small calculator thingy.

---
+++ TestCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCSHarp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CalculatorFun(1, 9);
+
+        }
+
+        static void CalculatorFun(int x, int y)
+        {
+            Console.WriteLine("I was asked the following question:");
+            Console.WriteLine("What is " + x + " + " + y + "???");
+            Console.WriteLine("I think it is like.. " + (x + y) + "... right?..");
+            Console.ReadKey();
+        }
+    }
+}
